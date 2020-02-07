3a08b7d904c84c30ff98efb57b6a69b9a0e900e7
Bill Brinkert <william.brinkert@akqa.com>
2020-02-07 11:03:50 -0800

    feat: update exercises 2 through 4

---
+++ 2-using-shared-components/components/MyLayout.js
@@ -0,0 +1,15 @@
+import Header from './Header'
+
+const layoutStyle = {
+  margin: '1.5rem',
+  padding: '1.25rem',
+  border: '1px solid #DDD'
+}
+
+const Layout = props => (
+  <div style={layoutStyle}>
+    <header />
+  </div>
+)
+
+export default Layout
